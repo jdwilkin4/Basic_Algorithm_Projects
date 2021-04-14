1cc8f69ef5be7fa3ebd53296eba71d808ce2b088
Jessica Wilkins <jwilkin4@hotmail.com>
2021-04-13 23:58:31 -0700

    adding reset button to weather converter

--- Celsius-to-Fahrenheit/Celsius-to-Fahrenheit.js
+++ Celsius-to-Fahrenheit/Celsius-to-Fahrenheit.js
@@ -1,12 +1,20 @@
 // lowest -90 highest 57
 
-const inputValue = document.getElementById('number').value;
+const inputValue = document.getElementById('number');
 const errorMsg = document.getElementById('error');
 const convertBtn = document.getElementById('convert');
 const result = document.getElementById('result');
+const reset = document.getElementById('reset');
+
 
 
 convertBtn.addEventListener("click", () => {
-    result.innerHTML = (inputValue * 9 / 5) + 32;
+    //work on error messages
+    let fahrenheit = Math.floor((inputValue.value * 9 / 5) + 32);
+    result.innerHTML = `${inputValue.value} degrees Celsius is ${fahrenheit} degrees Fahrenheit.`
 });
 
+reset.addEventListener('click', () => {
+    result.innerHTML = '';
+    inputValue.value = '';
+})
