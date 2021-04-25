4bf312f6ebdaa7b8c29f4d29f6c9db441a641259
Jessica Wilkins <jwilkin4@hotmail.com>
2021-04-25 01:19:05 -0700

    working on styles for celsius to fahrenheit project

--- Celsius-to-Fahrenheit/Celsius-to-Fahrenheit.js
+++ Celsius-to-Fahrenheit/Celsius-to-Fahrenheit.js
@@ -6,21 +6,25 @@ const reset = document.getElementById('reset');
 
 
 convertBtn.addEventListener("click", () => {
+    let fahrenheit = Math.floor((inputValue.value * 9 / 5) + 32);
+    const conversionString = `<p>${inputValue.value} degrees Celsius is</p>
+    <p>${fahrenheit} degrees Fahrenheit </p>`
 
     //checking for invalid user input
     if (inputValue.value > 57 || inputValue.value < -90 || inputValue.value === '') {
         result.innerHTML = `Invalid input. Try again.`
     } else if (inputValue.value >= 30 && inputValue.value <= 57) {
-        let fahrenheit = Math.floor((inputValue.value * 9 / 5) + 32);
-        result.innerHTML = `
-        <p>Ohh..that's HOT <i class="fas fa-fire"></i></p> 
-        <p>${inputValue.value} degrees Celsius is</p>
-        <p>${fahrenheit} degrees Fahrenheit </p>`
+        result.innerHTML = `<p class="result-text">Ohh..that's HOT <i class="fas fa-fire"></i></p>${conversionString}`
+    } else if (inputValue.value >= 20 && inputValue.value <= 29) {
+        result.innerHTML = `<p class="result-text">Not to bad<i class="far fa-sun"></i></p> ${conversionString}`
+    } else if (inputValue.value >= 0 && inputValue.value <= 19) {
+        result.innerHTML = `<p class="result-text">It's a little chilly <i class="fas fa-snowflake"></i></p> ${conversionString}`
     } else {
-        result.innerHTML = `${inputValue.value} degrees Celsius is ${fahrenheit} degrees Fahrenheit.`
+        result.innerHTML = `<p class="result-text">Wow, that's cold! <i class="fas fa-icicles"></i></p>${conversionString}`
     }
 
 });
+
 
 reset.addEventListener('click', () => {
     result.innerHTML = '';
