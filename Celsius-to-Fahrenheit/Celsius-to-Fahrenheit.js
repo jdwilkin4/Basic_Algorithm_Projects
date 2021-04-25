6692da75ab787ab64b8e52184a575f8927336519
Jessica Wilkins <jwilkin4@hotmail.com>
2021-04-24 23:53:37 -0700

    styling the celsius to fahrenheit project

--- Celsius-to-Fahrenheit/Celsius-to-Fahrenheit.js
+++ Celsius-to-Fahrenheit/Celsius-to-Fahrenheit.js
@@ -7,10 +7,16 @@ const reset = document.getElementById('reset');
 
 convertBtn.addEventListener("click", () => {
 
+    //checking for invalid user input
     if (inputValue.value > 57 || inputValue.value < -90 || inputValue.value === '') {
         result.innerHTML = `Invalid input. Try again.`
-    } else {
+    } else if (inputValue.value >= 30 && inputValue.value <= 57) {
         let fahrenheit = Math.floor((inputValue.value * 9 / 5) + 32);
+        result.innerHTML = `
+        <p>Ohh..that's HOT <i class="fas fa-fire"></i></p> 
+        <p>${inputValue.value} degrees Celsius is</p>
+        <p>${fahrenheit} degrees Fahrenheit </p>`
+    } else {
         result.innerHTML = `${inputValue.value} degrees Celsius is ${fahrenheit} degrees Fahrenheit.`
     }
 
@@ -18,6 +24,5 @@ convertBtn.addEventListener("click", () => {
 
 reset.addEventListener('click', () => {
     result.innerHTML = '';
-    errorMsg.innerHTML = '';
     inputValue.value = '';
 })
