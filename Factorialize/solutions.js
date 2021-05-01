c4ddffd8aae869a94514e4fb659c7b9ce2e95ed8
jdwilkin4 <67210629+jdwilkin4@users.noreply.github.com>
2021-05-01 16:28:29 -0700

    Update solutions.js

--- Factorialize/solutions.js
+++ Factorialize/solutions.js
@@ -1,1 +1,19 @@
 // recursive case
+const factorialize = (num) => {
+  
+  //base case
+  if (num === 0) {
+    return 1;
+    
+    //recursive case
+  } else {
+    return num * factorialize(num - 1);
+  }
+};
+
+factorialize(5);
+
+//recursive case using the ternary operator
+const factorialize = (num) =>  num === 0 ? 1 : num * factorialize(num - 1)
+
+factorialize(5);
